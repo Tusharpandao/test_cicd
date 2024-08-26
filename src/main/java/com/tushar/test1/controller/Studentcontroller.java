c9ebea7f2828def2cfa5c5e15122c73edbe00ff8
Tushar Pandao <pandaotushar1@gmail.com>
2024-08-26 19:05:25 +0530

    change the program''

---
+++ src/main/java/com/tushar/test1/controller/Studentcontroller.java
@@ -0,0 +1,15 @@
+package com.tushar.test1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Studentcontroller {
+
+	
+	@GetMapping("studentwelcome")
+	public String studnetwel() {
+	
+		return new String("Welcome to testing project");
+	}
+}
